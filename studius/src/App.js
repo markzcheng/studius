b1b7c4349a495de15ebc545f439c3b3b9e313ce7
hschawe <helen.schawe@gmail.com>
2021-04-06 19:46:16 -0500

    Login page

--- studius/src/App.js
+++ studius/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logoWithText from "./components/logo-with-text.png"
+
+// Define components
+
+class LogoWithText extends React.Component {
+  render() {
+      return (
+          <div class="containerCenter">
+            <img src={logoWithText} width="280" height="211"/>
+          </div>
+      );
+  }
+}
+
+class InputBox extends React.Component {
+  render() {
+    return (
+      <div class="container2">
+        <form>
+          <label for="e-mail"></label>
+          <input class="loginFormInput" type="text" id="e-mail" placeholder="e-mail" /><br/>
+          <label for="password"></label>
+          <input class="loginFormInput" type="password" id="password" placeholder="password" /><br/>
+        </form>
+      </div>
+    );
+  }
+}
+
+class SubmitButton extends React.Component {
+  render() {
+    return (
+      <div class="containerCenter">
+        <button class="submitButton loginButton" type="submit" onClick={function() { alert('click'); }}>
+          <h2>==></h2>
+        </button>
+      </div>
+    )
+  }
+}
+
+class RegisterButton extends React.Component {
+  render () {
+    return (
+      <div class="containerCenter">
+        <button class="registerButton">new to studius? register</button>
+      </div>
+    )
+  }
+}
+
+// Load components into screen
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="bottomMargin">
+        <LogoWithText />
+
+        <InputBox />
+
+        <SubmitButton />
+
+        <RegisterButton />
+      </div>
+
     </div>
   );
 }
