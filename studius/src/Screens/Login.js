23633d4e2d94b0c25fd190e5a6faca1210bb1408
hschawe <helen.schawe@gmail.com>
2021-04-07 18:45:08 -0500

    rename Screens folder

---
+++ studius/src/Screens/Login.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import '../App.css';
+import logoWithText from "../Assets/logo-with-text.png"
+
+// Define components
+
+class LogoWithText extends React.Component {
+  render() {
+      return (
+          <div class="containerCenter">
+            <img src={logoWithText} width="280" height="211"/>
+          </div>
+      );
+  }
+}
+
+class InputBox extends React.Component {
+  render() {
+    return (
+      <div class="container2">
+        <form>
+          <label for="e-mail"></label>
+          <input class="loginFormInput" type="text" id="e-mail" placeholder="e-mail" /><br/>
+          <label for="password"></label>
+          <input class="loginFormInput" type="password" id="password" placeholder="password" /><br/>
+        </form>
+      </div>
+    );
+  }
+}
+
+class SubmitButton extends React.Component {
+  render() {
+    return (
+      <div class="containerCenter">
+        <button class="submitButton loginButton" type="submit" onClick={function() { alert('click'); }}>
+          <h2>==></h2>
+        </button>
+      </div>
+    )
+  }
+}
+
+class RegisterButton extends React.Component {
+  render () {
+    return (
+      <div class="containerCenter">
+        <button class="registerButton">new to studius? register</button>
+      </div>
+    )
+  }
+}
+
+// Load components into screen
+
+function LoginScreen() {
+  return (
+    <div class="background">
+      <div class="container">
+        <div class="bottomMargin">
+          <LogoWithText />
+
+          <InputBox />
+
+          <SubmitButton />
+
+          <RegisterButton />
+
+          <div class="orText">or</div>
+          <div class="signInAlternatives">sign in with Google // Spotify</div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default LoginScreen;
