fd186c3732a04026af2929ea88648878a309c119
hschawe <helen.schawe@gmail.com>
2021-04-28 11:48:34 -0500

    Add timer without interactive functionality

--- studius/src/Screens/Study.js
+++ studius/src/Screens/Study.js
@@ -1,8 +1,58 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import "./Study.css";
+import colors from "../Assets/colors.js"
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+
+const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+        return <div className="timer">Done!</div>;
+    }
+  
+    return (
+        <div className="timer">
+            <div className="value">{remainingTime}</div>
+            <div className="text">seconds</div>
+      </div>
+    );
+};
+
 
 function StudyScreen() {
     return (
-        <div>Study</div>
+        <div className="App">
+            <h1 className="top-text">start focusing</h1>
+
+            <div className="timer-wrapper">
+                <div className="timer-box">
+                    <CountdownCircleTimer
+                        isPlaying
+                        duration={10}
+                        colors={[[colors.darkPurple]]}
+                        onComplete={() => [true, 1000]}
+                        size={250}
+                        strokeWidth={18}
+                        rotation={"counterclockwise"}
+                    >
+                        {renderTime}
+                    </CountdownCircleTimer>
+                </div>
+            </div>
+            
+            <p className="info">
+                up next:
+                5 minute break
+            </p>
+
+            <div className="buttonHolder"> 
+                <button className="historyButton" onClick={function() { alert('click'); }}>history</button>
+            </div>
+            <div className="buttonHolder"> 
+                <button className="resetButton" onClick={function() { alert('click'); }}>reset</button>
+            </div>
+
+        </div>
     );
 }
 
