3e25e69b315c362283aea23be62ea8cef67ac50f
hschawe <helen.schawe@gmail.com>
2021-05-07 16:43:37 -0500

    make timer pretty

--- studius/src/Screens/Study.js
+++ studius/src/Screens/Study.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
 import "./Study.css";
 import colors from "../Assets/colors.js"
@@ -13,30 +13,37 @@ const renderTime = ({ remainingTime }) => {
     return (
         <div className="timer">
             <div className="value">{remainingTime}</div>
-            <div className="text">seconds</div>
+            <h4>seconds</h4>
       </div>
     );
 };
 
 
 function StudyScreen() {
+    const [timerIsOn, setTimerIsOn] = useState(false);
+    const [key, setKey] = useState(0);
+
     return (
         <div className="App">
             <h1 className="top-text">start focusing</h1>
 
             <div className="timer-wrapper">
                 <div className="timer-box">
-                    <CountdownCircleTimer
-                        isPlaying
-                        duration={10}
-                        colors={[[colors.darkPurple]]}
-                        onComplete={() => [true, 1000]}
-                        size={250}
-                        strokeWidth={18}
-                        rotation={"counterclockwise"}
-                    >
-                        {renderTime}
-                    </CountdownCircleTimer>
+                    <div className="timer-bg">
+                        <CountdownCircleTimer
+                            key={key}
+                            isPlaying={timerIsOn}
+                            duration={10}
+                            colors={[[colors.darkPurple]]}
+                            trailColor={[colors.lightPurple]}
+                            onComplete={() => [false, 1500]}
+                            size={250}
+                            strokeWidth={18}
+                            rotation={"counterclockwise"}
+                        >
+                            {renderTime}
+                        </CountdownCircleTimer>
+                    </div>
                 </div>
             </div>
             
@@ -46,10 +53,14 @@ function StudyScreen() {
             </p>
 
             <div className="buttonHolder"> 
-                <button className="historyButton" onClick={function() { alert('click'); }}>history</button>
+                <button className="startButton" onClick={() => setTimerIsOn(true)}>start</button>
+                <button className="pauseButton" onClick={() => setTimerIsOn(false)}>pause</button>
             </div>
+
+
             <div className="buttonHolder"> 
-                <button className="resetButton" onClick={function() { alert('click'); }}>reset</button>
+                <button className="historyButton" onClick={function() { alert('click'); }}>history</button>
+                <button className="resetButton" onClick={() => setKey(prevKey => prevKey + 1)}>reset</button>
             </div>
 
         </div>
